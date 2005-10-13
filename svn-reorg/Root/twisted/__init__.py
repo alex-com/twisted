090c3425f9effde3ae505cc7cdee42731940dd55
foom <foom@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2005-10-13 01:23:04 +0000

    The little wrapperthingy.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/foom@14835 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

---
+++ svn-reorg/Root/twisted/__init__.py
@@ -0,0 +1,13 @@
+"""Glue package for developers to use split out projects as one"""
+import os.path, glob
+__path__ = ["core", "conch", "flow", "lore", "mail", 
+                       "names", "news", "pair", "runner", "web", 
+                       "web2", "words", "xish"]
+__path__=[os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                       "..", "..", elt, "twisted"))
+          for elt in __path__]
+import copyright
+__version__ = copyright.version
+
+from twisted.python import compat
+del compat, os
