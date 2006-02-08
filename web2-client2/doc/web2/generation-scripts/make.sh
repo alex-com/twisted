676add7e4b9da40a8bd78c88c8ca792c7e46de6a
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2006-02-08 02:46:00 +0000

    merged forward, obsolete
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/foom@15845 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- web2-client2/doc/web2/generation-scripts/make.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd `dirname $0`
-python txt2html.py .. ../html en
-
--- web2-client2/twisted/words/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- test-case-name: twisted.words.test -*-
-# Copyright (c) 2001-2004 Twisted Matrix Laboratories.
-# See LICENSE for details.
-
-
-"""Twisted Words: a Twisted Chat service.
-"""
-
-__version__ = 'SVN-Trunk'
