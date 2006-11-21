alex-com/twisted

commit 8bd4177fe0dd1ee5016d59c752c01b1bbb296c2c

Use `LoggedSuite`  instead of `startKeepingErrors` to trap logged errors  * Author: jml  * Reviewers: Jerub, exarkun  * Fixes #2173
`startKeepingErrors` has been deprecated for a while, but Trial was still
using it to catch errors that were logged during a test run, but outside 
of any particular test case. 

This branch creates a new kind of `TestSuite` called `LoggedSuite`, which
monitors the log and adds any logged errors to the test result object.



git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@18764 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

twisted/trial/runner.py
@@ -149,6 +149,37 @@ class TestSuite(pyunit.TestSuite):
         return result
 
 
+
+# When an error occurs outside of any test, the user will see this string
+# in place of a test's name.
+NOT_IN_TEST = "<not in test>"
+
+
+
+class LoggedSuite(TestSuite):
+    """
+    Any errors logged in this suite will be reported to the L{TestResult}
+    object.
+    """
+
+    def run(self, result):
+        """
+        Run the suite, storing all errors in C{result}. If an error is logged
+        while no tests are running, then it will be added as an error to
+        C{result}.
+
+        @param result: A L{TestResult} object.
+        """
+        observer = unittest._logObserver
+        observer._add()
+        super(LoggedSuite, self).run(result)
+        observer._remove()
+        for error in observer.getErrors():
+            result.addError(TestHolder(NOT_IN_TEST), error)
+        observer.flushErrors()
+
+
+
 class DocTestSuite(TestSuite):
     """
     Behaves like doctest.DocTestSuite, but decorates individual TestCases so
@@ -223,6 +254,11 @@ class TrialSuite(TestSuite):
     what context they are run in.
     """
 
+    def __init__(self, tests=()):
+        suite = LoggedSuite(tests)
+        super(TrialSuite, self).__init__([suite])
+
+
     def _bail(self):
         from twisted.internet import reactor
         d = defer.Deferred()
@@ -240,7 +276,6 @@ class TrialSuite(TestSuite):
 
     def run(self, result):
         try:
-            log.startKeepingErrors()
             TestSuite.run(self, result)
         finally:
             self._bail()
@@ -281,12 +316,35 @@ def isTestCase(obj):
         return False
 
 
-class ErrorHolder(object):
+
+class TestHolder(object):
+    """
+    Placeholder for a L{TestCase} inside a reporter. As far as a L{TestResult}
+    is concerned, this looks exactly like a unit test.
+    """
+
+    def __init__(self, description):
+        """
+        @param description: A string to be displayed L{TestResult}.
+        """
+        self.description = description
+
+
+    def id(self):
+        return self.description
+
+
+    def shortDescription(self):
+        return self.description
+
+
+
+class ErrorHolder(TestHolder):
     """
     Used to insert arbitrary errors into a test suite run. Provides enough
-    methods to like a C{TestCase}, however, when it is run, it simply adds an
-    error to the C{TestResult}. The most common use-case is for when a module
-    fails to import.
+    methods to look like a C{TestCase}, however, when it is run, it simply adds
+    an error to the C{TestResult}. The most common use-case is for when a
+    module fails to import.
     """
 
     def __init__(self, description, error):
@@ -297,14 +355,8 @@ class ErrorHolder(object):
         @param error: The error to be added to the result. Can be an exc_info
         tuple or a L{twisted.python.failure.Failure}.
         """
-        self.description = description
+        super(ErrorHolder, self).__init__(description)
         self.error = error
-
-    def id(self):
-        return self.description
-
-    def shortDescription(self):
-        return self.description
 
     def __repr__(self):
         return "<ErrorHolder description=%r error=%r>" % (self.description,
@@ -322,6 +374,7 @@ class ErrorHolder(object):
     def visit(self, visitor):
         # This needs tests.
         visitor.visitCase(self)
+
 
 
 class TestLoader(object):

commit 07f140b1f4d14db8d8244e68e2ce2826d555c93c

Use `LoggedSuite`  instead of `startKeepingErrors` to trap logged errors  * Author: jml  * Reviewers: Jerub, exarkun  * Fixes #2173  * Remerged with fix for test regression caused by r18764
`startKeepingErrors` has been deprecated for a while, but Trial was still
using it to catch errors that were logged during a test run, but outside
of any particular test case.

This branch creates a new kind of `TestSuite` called `LoggedSuite`, which
monitors the log and adds any logged errors to the test result object.




git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/trunk@18767 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

twisted/trial/runner.py
@@ -149,6 +149,37 @@ class TestSuite(pyunit.TestSuite):
         return result
 
 
+
+# When an error occurs outside of any test, the user will see this string
+# in place of a test's name.
+NOT_IN_TEST = "<not in test>"
+
+
+
+class LoggedSuite(TestSuite):
+    """
+    Any errors logged in this suite will be reported to the L{TestResult}
+    object.
+    """
+
+    def run(self, result):
+        """
+        Run the suite, storing all errors in C{result}. If an error is logged
+        while no tests are running, then it will be added as an error to
+        C{result}.
+
+        @param result: A L{TestResult} object.
+        """
+        observer = unittest._logObserver
+        observer._add()
+        super(LoggedSuite, self).run(result)
+        observer._remove()
+        for error in observer.getErrors():
+            result.addError(TestHolder(NOT_IN_TEST), error)
+        observer.flushErrors()
+
+
+
 class DocTestSuite(TestSuite):
     """
     Behaves like doctest.DocTestSuite, but decorates individual TestCases so
@@ -223,6 +254,11 @@ class TrialSuite(TestSuite):
     what context they are run in.
     """
 
+    def __init__(self, tests=()):
+        suite = LoggedSuite(tests)
+        super(TrialSuite, self).__init__([suite])
+
+
     def _bail(self):
         from twisted.internet import reactor
         d = defer.Deferred()
@@ -240,7 +276,6 @@ class TrialSuite(TestSuite):
 
     def run(self, result):
         try:
-            log.startKeepingErrors()
             TestSuite.run(self, result)
         finally:
             self._bail()
@@ -281,12 +316,35 @@ def isTestCase(obj):
         return False
 
 
-class ErrorHolder(object):
+
+class TestHolder(object):
+    """
+    Placeholder for a L{TestCase} inside a reporter. As far as a L{TestResult}
+    is concerned, this looks exactly like a unit test.
+    """
+
+    def __init__(self, description):
+        """
+        @param description: A string to be displayed L{TestResult}.
+        """
+        self.description = description
+
+
+    def id(self):
+        return self.description
+
+
+    def shortDescription(self):
+        return self.description
+
+
+
+class ErrorHolder(TestHolder):
     """
     Used to insert arbitrary errors into a test suite run. Provides enough
-    methods to like a C{TestCase}, however, when it is run, it simply adds an
-    error to the C{TestResult}. The most common use-case is for when a module
-    fails to import.
+    methods to look like a C{TestCase}, however, when it is run, it simply adds
+    an error to the C{TestResult}. The most common use-case is for when a
+    module fails to import.
     """
 
     def __init__(self, description, error):
@@ -297,14 +355,8 @@ class ErrorHolder(object):
         @param error: The error to be added to the result. Can be an exc_info
         tuple or a L{twisted.python.failure.Failure}.
         """
-        self.description = description
+        super(ErrorHolder, self).__init__(description)
         self.error = error
-
-    def id(self):
-        return self.description
-
-    def shortDescription(self):
-        return self.description
 
     def __repr__(self):
         return "<ErrorHolder description=%r error=%r>" % (self.description,
@@ -322,6 +374,7 @@ class ErrorHolder(object):
     def visit(self, visitor):
         # This needs tests.
         visitor.visitCase(self)
+
 
 
 class TestLoader(object):

twisted/trial/unittest.py
@@ -43,11 +43,11 @@ class Todo(object):
     category. If todo'd tests succeed, Trial L{TestResult}s will report an
     unexpected success.
     """
-    
+
     def __init__(self, reason, errors=None):
         """
         @param reason: A string explaining why the test is marked 'todo'
-        
+
         @param errors: An iterable of exception types that the test is
         expected to raise. If one of these errors is raised by the test, it
         will be trapped. Raising any other kind of error will fail the test.
@@ -106,7 +106,7 @@ class _Assertions(pyunit.TestCase, object):
     Although the tests are defined as 'failIf*' and 'failUnless*', they can
     also be called as 'assertNot*' and 'assert*'.
     """
-    
+
     def fail(self, msg=None):
         """absolutely fails the test, do not pass go, do not collect $200
 
@@ -127,7 +127,7 @@ class _Assertions(pyunit.TestCase, object):
 
     def failUnless(self, condition, msg=None):
         """fails the test if C{condition} evaluates to True
-        
+
         @param condition: any object that defines __nonzero__
         """
         if not condition:
@@ -139,10 +139,10 @@ class _Assertions(pyunit.TestCase, object):
         """fails the test unless calling the function C{f} with the given C{args}
         and C{kwargs} does not raise C{exception}. The failure will report the
         traceback and call stack of the unexpected exception.
-        
+
         @param exception: exception type that is to be expected
         @param f: the function to call
-    
+
         @return: The raised exception instance, if it is of the given type.
         @raise self.failureException: Raised if the function call does not raise an exception
         or if it raises an exception of a different type.
@@ -174,7 +174,7 @@ class _Assertions(pyunit.TestCase, object):
     def failUnlessIdentical(self, first, second, msg=None):
         """fail the test if C{first} is not C{second}. This is an
         obect-identity-equality test, not an object equality (i.e. C{__eq__}) test
-        
+
         @param msg: if msg is None, then the failure message will be
         '%r is not %r' % (first, second)
         """
@@ -186,7 +186,7 @@ class _Assertions(pyunit.TestCase, object):
     def failIfIdentical(self, first, second, msg=None):
         """fail the test if C{first} is C{second}. This is an
         obect-identity-equality test, not an object equality (i.e. C{__eq__}) test
-        
+
         @param msg: if msg is None, then the failure message will be
         '%r is %r' % (first, second)
         """
@@ -197,7 +197,7 @@ class _Assertions(pyunit.TestCase, object):
 
     def failIfEqual(self, first, second, msg=None):
         """fail the test if C{first} == C{second}
-        
+
         @param msg: if msg is None, then the failure message will be
         '%r == %r' % (first, second)
         """
@@ -253,7 +253,7 @@ class _Assertions(pyunit.TestCase, object):
         return first
     assertNotAlmostEqual = assertNotAlmostEquals = failIfAlmostEqual
     failIfAlmostEquals = failIfAlmostEqual
-    
+
     def failUnlessAlmostEqual(self, first, second, places=7, msg=None):
         """Fail if the two objects are unequal as determined by their
         difference rounded to the given number of decimal places
@@ -421,7 +421,7 @@ class TestCase(_Assertions):
 
     @ivar forceGarbageCollection: If set to True, C{gc.collect()} will be
     called before and after the test. Otherwise, garbage collection will
-    happen in whatever way Python sees fit.    
+    happen in whatever way Python sees fit.
 
     @ivar skip: C{None} or a string explaining why this test is to be
     skipped. If defined, the test will not be run. Instead, it will be
@@ -441,7 +441,7 @@ class TestCase(_Assertions):
     classes, and C{reason} is a string. See L{Todo} or L{makeTodo} for more
     information.
     """
-    
+
     zi.implements(itrial.ITestCase)
     failureException = FailTest
 
@@ -488,7 +488,7 @@ class TestCase(_Assertions):
         if not hasattr(self.__class__, '_testCaseInstance'):
             self.__class__._testCaseInstance = self
         if self.__class__._testCaseInstance.__class__ != self.__class__:
-            self.__class__._testCaseInstance = self            
+            self.__class__._testCaseInstance = self
 
     def _run(self, methodName, result):
         from twisted.internet import reactor
@@ -754,7 +754,7 @@ class TestCase(_Assertions):
                 self._wait(d)
             finally:
                 self._cleanUp(result)
-                result.stopTest(self)            
+                result.stopTest(self)
                 if self._shared and self._isLast():
                     self._initInstances()
                     self._classCleanUp(result)
@@ -826,7 +826,7 @@ class TestCase(_Assertions):
         anything. See L{TestCase} docstring for more details.
         """
         return util.acquireAttribute(self._parents, 'suppress', [])
-    
+
     def visit(self, visitor):
         """Call visitor.visitCase(self)."""
         visitor.visitCase(self)
@@ -834,7 +834,7 @@ class TestCase(_Assertions):
     def mktemp(self):
         """Returns a unique name that may be used as either a temporary
         directory or filename.
-        
+
         @note: you must call os.mkdir on the value returned from this
                method if you wish to use it as a directory!
         """
@@ -846,14 +846,14 @@ class TestCase(_Assertions):
             os.makedirs(base)
         dirname = tempfile.mkdtemp('', '', base)
         return os.path.join(dirname, 'temp')
-    
+
     def _wait(self, d, running=_wait_is_running):
         """Take a Deferred that only ever callbacks. Block until it happens.
         """
         from twisted.internet import reactor
         if running:
             raise RuntimeError("_wait is not reentrant")
-    
+
         results = []
         def append(any):
             if results is not None:
@@ -863,13 +863,13 @@ class TestCase(_Assertions):
                 reactor.crash()
         crash = utils.suppressWarnings(
             crash, util.suppress(message=r'reactor\.crash cannot be used.*',
-                                 category=DeprecationWarning)) 
+                                 category=DeprecationWarning))
         def stop():
             reactor.crash()
         stop = utils.suppressWarnings(
             stop, util.suppress(message=r'reactor\.crash cannot be used.*',
-                                category=DeprecationWarning)) 
-    
+                                category=DeprecationWarning))
+
         running.append(None)
         try:
             d.addBoth(append)
@@ -883,7 +883,7 @@ class TestCase(_Assertions):
                 reactor.run()
             finally:
                 del reactor.stop
-    
+
             # If the reactor was crashed elsewhere due to a timeout, hopefully
             # that crasher also reported an error. Just return.
             # _timedOut is most likely to be set when d has fired but hasn't
@@ -894,7 +894,7 @@ class TestCase(_Assertions):
             # If the timeout didn't happen, and we didn't get a result or
             # a failure, then the user probably aborted the test, so let's
             # just raise KeyboardInterrupt.
-    
+
             # FIXME: imagine this:
             # web/test/test_webclient.py:
             # exc = self.assertRaises(error.Error, wait, method(url))
@@ -921,7 +921,7 @@ class PyUnitResultAdapter(object):
     L{twisted.python.failure.Failure}s being passed to L{addError} and
     L{addFailure}.
     """
-    
+
     def __init__(self, original):
         """
         @param original: A C{TestResult} instance from C{unittest}.
@@ -950,9 +950,9 @@ class PyUnitResultAdapter(object):
 
     def _unsupported(self, test, feature, info):
         self.original.addFailure(
-            test, 
-            (UnsupportedTrialFeature, 
-             UnsupportedTrialFeature(feature, info), 
+            test,
+            (UnsupportedTrialFeature,
+             UnsupportedTrialFeature(feature, info),
              None))
 
     def addSkip(self, test, reason):
@@ -966,7 +966,7 @@ class PyUnitResultAdapter(object):
         Report the unexpected success as a failure.
         """
         self._unsupported(test, 'unexpected success', todo)
-        
+
     def addExpectedFailure(self, test, error):
         """
         Report the expected failure (i.e. todo) as a failure.
@@ -981,13 +981,13 @@ class PyUnitResultAdapter(object):
 
     def cleanupErrors(self, errs):
         pass
-    
+
     def startSuite(self, name):
         pass
 
 
 class TestVisitor(object):
-    
+
     def visitCase(self, testCase):
         """Visit the testCase testCase."""
 
@@ -1007,7 +1007,7 @@ class TestVisitor(object):
 class _SubTestCase(TestCase):
     def __init__(self):
         TestCase.__init__(self, 'run')
-    
+
 _inst = _SubTestCase()
 
 def deprecate(name):

twisted/trial/test/test_runner.py
@@ -10,7 +10,7 @@ from zope.interface import implements
 
 from twisted.trial.itrial import IReporter
 from twisted.trial import unittest, runner, reporter
-from twisted.python import reflect
+from twisted.python import failure, log, reflect
 from twisted.scripts import trial
 from twisted.plugins import twisted_trial
 from twisted import plugin
@@ -39,7 +39,7 @@ class CapturingReporter(object):
         """Create a capturing reporter."""
         self._calls = []
         self.shouldStop = False
-        
+
     def startTest(self, method):
         """report the beginning of a run of a single test method
         @param method: an object that is adaptable to ITestMethod
@@ -86,22 +86,22 @@ class CapturingReporter(object):
         pass
 
 
-class TestImports(unittest.TestCase):
-
-    def test_imports(self):
-        from twisted.trial.runner import TrialRunner
-
 
 class TestTrialRunner(unittest.TestCase):
 
     def setUp(self):
         self.stream = StringIO.StringIO()
         self.runner = runner.TrialRunner(CapturingReporter, stream=self.stream)
-        self.test = TestImports('test_imports')
+        self.test = TestTrialRunner('test_empty')
+
+    def test_empty(self):
+        """
+        Empty test method, used by the other tests.
+        """
 
     def tearDown(self):
         self.runner._tearDownLogFile()
-        
+
     def _getObservers(self):
         from twisted.python import log
         return log.theLogPublisher.observers
@@ -111,7 +111,7 @@ class TestTrialRunner(unittest.TestCase):
         self.runner.run(self.test)
         newCount = len(self._getObservers())
         self.failUnlessEqual(originalCount + 2, newCount)
-        
+
     def test_addObservers_repeat(self):
         self.runner.run(self.test)
         count = len(self._getObservers())
@@ -134,10 +134,10 @@ class TestTrialRunner(unittest.TestCase):
         self.failUnless(fd.closed)
 
 class TestRunner(unittest.TestCase):
-    
+
     def setUp(self):
         unittest.TestCase.setUp(self)
-        self.runners = []        
+        self.runners = []
         self.config = trial.Options()
         # whitebox hack a reporter in, because plugins are CACHED and will
         # only reload if the FILE gets changed.
@@ -225,7 +225,7 @@ class TestRunner(unittest.TestCase):
         r = trial._makeRunner(self.config)
         self.runners.append(r)
         return r
-    
+
     def test_runner_can_get_reporter(self):
         self.parseOptions([])
         reporter = self.config['reporter']
@@ -283,12 +283,15 @@ class TestRunner(unittest.TestCase):
         self.assertEqual(['runcall'], debugger._calls)
 
 
+
 class TestTrialSuite(unittest.TestCase):
 
     def test_imports(self):
+        # FIXME, HTF do you test the reactor can be cleaned up ?!!!
         from twisted.trial.runner import TrialSuite
+        # silence pyflakes warning
+        silencePyflakes = TrialSuite
 
-    # FIXME, HTF do you test the reactor can be cleaned up ?!!!
 
 
 class TestUntilFailure(unittest.TestCase):
@@ -310,4 +313,84 @@ class TestUntilFailure(unittest.TestCase):
         self.failUnlessEqual(result.testsRun, 1)
         self.failIf(result.wasSuccessful())
         self.failUnlessEqual(len(result.failures), 1)
-        
+
+
+
+class BreakingSuite(runner.TestSuite):
+    """
+    A L{TestSuite} that logs an error when it is run.
+    """
+
+    def run(self, result):
+        try:
+            raise RuntimeError("error that occurs outside of a test")
+        except RuntimeError, e:
+            log.err(failure.Failure())
+
+
+
+class TestLoggedErrors(unittest.TestCase):
+    """
+    It is possible for an error generated by a test to be logged I{outside} of
+    any test. The log observers constructed by L{TestCase} won't catch these
+    errors. Here we try to generate such errors and ensure they are reported to
+    a L{TestResult} object.
+    """
+
+    def tearDown(self):
+        self.flushLoggedErrors(RuntimeError)
+
+
+    def test_construct(self):
+        """
+        Check that we can construct a L{runner.LoggedSuite} and that it
+        starts empty.
+        """
+        suite = runner.LoggedSuite()
+        self.assertEqual(suite.countTestCases(), 0)
+
+
+    def test_capturesError(self):
+        """
+        Chek that a L{LoggedSuite} reports any logged errors to its result.
+        """
+        result = reporter.TestResult()
+        suite = runner.LoggedSuite([BreakingSuite()])
+        suite.run(result)
+        self.assertEqual(len(result.errors), 1)
+        self.assertEqual(result.errors[0][0].id(), runner.NOT_IN_TEST)
+        self.failUnless(result.errors[0][1].check(RuntimeError))
+
+
+
+class TestTestHolder(unittest.TestCase):
+
+    def setUp(self):
+        self.description = "description"
+        self.holder = runner.TestHolder(self.description)
+
+
+    def test_holder(self):
+        """
+        Check that L{runner.TestHolder} takes a description as a parameter
+        and that this description is returned by the C{id} and
+        C{shortDescription} methods.
+        """
+        self.assertEqual(self.holder.id(), self.description)
+        self.assertEqual(self.holder.shortDescription(), self.description)
+
+
+
+class TestErrorHolder(TestTestHolder):
+    """
+    Test L{runner.ErrorHolder} shares behaviour with L{runner.TestHolder}.
+    """
+
+    def setUp(self):
+        self.description = "description"
+        # make a real Failure so we can construct ErrorHolder()
+        try:
+            1/0
+        except ZeroDivisionError:
+            error = failure.Failure()
+        self.holder = runner.ErrorHolder(self.description, error)