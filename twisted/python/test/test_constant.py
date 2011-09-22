b3a2d184d233e36101cb1084bb75a6931ecbf70c
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2011-09-22 20:08:08 +0000

    More test conversion - and discover a mistake in the API
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/constants-4671@32570 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/python/test/test_constant.py
+++ twisted/python/test/test_constant.py
@@ -240,18 +240,11 @@ class SequenceTests(TestCase):
 
 FXF = bitvector.FXF(u"READ", u"WRITE", u"APPEND")
 
-assert FXF[1 | 2] is (FXF.READ | FXF.WRITE)
-assert 8 not in FXF
-assert 9 not in FXF
 
 assert ~FXF.READ is (FXF.WRITE | FXF.APPEND)
 assert (FXF.READ | FXF.WRITE) & (FXF.WRITE | FXF.APPEND) is FXF.WRITE
 assert (FXF.READ | FXF.WRITE) ^ (FXF.WRITE | FXF.APPEND) is (FXF.READ | FXF.APPEND)
 
-assert repr(FXF.READ) == "<FXF: READ>"
-assert repr(FXF.READ | FXF.WRITE) == "<FXF: READ, WRITE>"
-
-assert (FXF.READ | FXF.WRITE) == 1 | 2
 
 class BitvectorTests(TestCase):
     """
@@ -295,9 +288,6 @@ class BitvectorTests(TestCase):
         self.assertEqual(3, len(FXF))
 
 
-    def test
-
-
     def test_or(self):
         """
         Two bitvector constants can be combined using C{|}, producing a new
@@ -307,6 +297,72 @@ class BitvectorTests(TestCase):
         self.assertEqual(5, FXF.READ | FXF.APPEND)
         self.assertEqual(6, FXF.WRITE | FXF.APPEND)
         self.assertEqual(7, FXF.READ | FXF.WRITE | FXF.APPEND)
+
+
+    def test_combinedRepresentation(self):
+        """
+        The object resulting from the combination of two bitvector constants
+        using C{|} has a string representation reflecting both of the inputs.
+        """
+        self.assertEqual("<FXF=READ|WRITE>", FXF.READ | FXF.WRITE)
+
+
+    def test_negation(self):
+        """
+        The negation of one of the constants created by L{bitvector} is the
+        disjunction of all the other constants in that L{bitvector}.
+        """
+        self.assertIdentical(~FXF.READ, FXF.WRITE | FXF.APPEND)
+
+
+    def test_and(self):
+        """
+        Two bitvector constants can be combined using C{&}, producing a new
+        constant representing the conjunction of the inputs.
+        """
+        self.assertEqual(1, FXF.READ & (FXF.READ | FXF.WRITE))
+        self.assertEqual(2, FXF.WRITE & (FXF.READ | FXF.WRITE))
+        self.assertEqual(
+            3, (FXF.READ | FXF.READ) & (FXF.READ | FXF.WRITE | FXF.APPEND))
+        self.assertEqual(0, FXF.READ & FXF.WRITE) # XXX GLUGH
+
+
+    def test_xor(self):
+        """
+        The result of an exclusive or operation on two L{bitvector} constants is
+        a constant with exactly the bits set which were in one or the other but
+        not both inputs.
+        """
+        self.assertIdentical(FXF.READ ^ (FXF.READ | FXF.WRITE), FXF.WRITE)
+        self.assertIdentical(
+            (FXF.READ | FXF.WRITE) ^ (FXF.WRITE | FXF.APPEND),
+            (FXF.READ | FXF.APPEND))
+        self.assertIdentical(FXF.READ ^ FXF.READ, 0) # XXX OOPS API FAIL
+
+
+    def test_containsBits(self):
+        """
+        The object returned by L{bitvector} contains the integer values of the
+        constants it defines and the integer values of combinations of the
+        constants it defines.
+        """
+        self.assertIn(1, FXF)
+        self.assertIn(2, FXF)
+        self.assertIn(1 | 2, FXF)
+        self.assertIn(4, FXF)
+        self.assertIn(1 | 4, FXF)
+        self.assertIn(2 | 4, FXF)
+        self.assertIn(1 | 2 | 4, FXF)
+
+
+    def test_withoutOtherContents(self):
+        """
+        The object returned by L{bitvector} does not contain other integer
+        values.
+        """
+        self.assertNotIn(0, FXF)
+        self.assertNotIn(8, FXF)
+        self.assertNotIn(9, FXF)
 
 
     def test_lookupByValue(self):
@@ -324,10 +380,10 @@ class BitvectorTests(TestCase):
         Indexing a bitvector object by a value corresponding to the combination
         of two or more of its constants results in
         """
-        self.assertIdentical(FXF[3], FXF.READ | FXF.WRITE)
-        self.assertIdentical(FXF[5], FXF.READ | FXF.APPEND)
-        self.assertIdentical(FXF[6], FXF.WRITE | FXF.WRITE)
-        self.assertIdentical(FXF[7], FXF.READ | FXF.WRITE | FXF.WRITE)
+        self.assertIdentical(FXF[1 | 2], FXF.READ | FXF.WRITE)
+        self.assertIdentical(FXF[1 | 4], FXF.READ | FXF.APPEND)
+        self.assertIdentical(FXF[2 | 4], FXF.WRITE | FXF.APPEND)
+        self.assertIdentical(FXF[1 | 2 | 4], FXF.READ | FXF.WRITE | FXF.WRITE)
 
 
 
