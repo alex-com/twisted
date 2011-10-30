df9e2cbe8e5bedb2f5210d8d24c2f1601d6ca0e0
cyli <cyli@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2011-10-30 00:46:11 +0000

    Add auth mixin tests refs #4752
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/ftp-AuthOptionMixin-4752-2@33008 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/test/test_ftp_options.py
+++ twisted/test/test_ftp_options.py
@@ -7,6 +7,7 @@ Tests for L{twisted.tap.ftp}.
 
 from twisted.trial.unittest import TestCase
 
+from twisted.cred import credentials, error
 from twisted.tap.ftp import Options
 from twisted.python import versions
 from twisted.python.filepath import FilePath
@@ -17,14 +18,59 @@ class FTPOptionsTestCase(TestCase):
     Tests for the command line option parser used for C{twistd ftp}.
     """
 
+    usernamePassword = ('iamuser', 'thisispassword')
+
+    def setUp(self):
+        """
+        Create a file with two users.
+        """
+        self.filename = self.mktemp()
+        f = FilePath(self.filename)
+        f.setContent(':'.join(self.usernamePassword))
+        self.options = Options()
+
+
     def test_passwordfileDeprecation(self):
         """
         Test that the --passwordfile option will emit a warning stating that
         said option is deprecated.
         """
-        passwd = FilePath(self.mktemp())
-        passwd.setContent("usernameTest:passwordTest")
-        options = Options()
         self.callDeprecated(
             versions.Version("Twisted", 11, 1, 0),
-            options.opt_password_file, passwd.path)
+            self.options.opt_password_file, self.filename)
+
+
+    def test_authAdded(self):
+        """
+        Tests that the --auth command generates a checker.
+        """
+        numCheckers = len(self.options['credCheckers'])
+        self.options.parseOptions(['--auth', 'file:' + self.filename])
+        self.assertEqual(len(self.options['credCheckers']), numCheckers + 1)
+
+
+    def test_authFailure(self):
+        """
+        Tests that the --auth command generates a checker does not authorize
+        invalid logins
+        """
+        self.options.parseOptions(['--auth', 'file:' + self.filename])
+        checker = self.options['credCheckers'][-1]
+        invalid = credentials.UsernamePassword(self.usernamePassword[0], 'fake')
+        return (checker.requestAvatarId(invalid)
+            .addCallbacks(
+                lambda ignore: self.fail("Wrong password should raise error"),
+                lambda err: err.trap(error.UnauthorizedLogin)))
+
+
+    def test_authSuccess(self):
+        """
+        Tests that the --auth command generates a checker that authorizes valid
+        logins
+        """
+        self.options.parseOptions(['--auth', 'file:' + self.filename])
+        checker = self.options['credCheckers'][-1]
+        correct = credentials.UsernamePassword(*self.usernamePassword)
+        return checker.requestAvatarId(correct).addCallback(
+            lambda username: self.assertEqual(username, correct.username)
+        )
