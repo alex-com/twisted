3343b6f2c58be7c0632f675d2fddb673372ee004
exarkun <exarkun@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2011-10-31 21:26:14 +0000

    Go back to packing the fileno into a string, with platform detection to decide how long to make the string
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/iocp-win64-4669@33020 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/internet/iocpreactor/tcp.py
+++ twisted/internet/iocpreactor/tcp.py
@@ -34,6 +34,10 @@ connectExErrors = {
         ERROR_NETWORK_UNREACHABLE: errno.WSAENETUNREACH,
         }
 
+if struct.calcsize('P') == 4:
+    _CONTEXT_VALUE_FORMAT = 'L'
+else:
+    _CONTEXT_VALUE_FORMAT == 'Q'
 
 class Connection(abstract.FileHandle, _SocketCloser, _AbortingMixin):
     """
@@ -279,7 +283,8 @@ class Client(Connection):
                                     errno.errorcode.get(rc, 'Unknown error'))))
         else:
             self.socket.setsockopt(
-                socket.SOL_SOCKET, SO_UPDATE_CONNECT_CONTEXT, 1)
+                socket.SOL_SOCKET, SO_UPDATE_CONNECT_CONTEXT,
+                struct.pack(_CONTEXT_VALUE_FORMAT, self.socket.fileno()))
             self.protocol = self.connector.buildProtocol(self.getPeer())
             self.connected = True
             logPrefix = self._getLogPrefix(self.protocol)
@@ -560,7 +565,8 @@ class Port(_SocketCloser, _LogOwner):
             return False
         else:
             evt.newskt.setsockopt(
-                socket.SOL_SOCKET, SO_UPDATE_ACCEPT_CONTEXT, 1)
+                socket.SOL_SOCKET, SO_UPDATE_ACCEPT_CONTEXT,
+                struct.pack(_CONTEXT_VALUE_FORMAT, self.socket.fileno()))
             family, lAddr, rAddr = _iocp.get_accept_addrs(evt.newskt.fileno(),
                                                           evt.buff)
             assert family == self.addressFamily
