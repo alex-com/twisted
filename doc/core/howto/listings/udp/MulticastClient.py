f2c952f84bb475782ec28819909ebd70ddea45dd
itamarst <itamarst@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2011-11-10 02:10:36 +0000

    Apply patch.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/multicast-doc-4262@33100 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- doc/core/howto/listings/udp/MulticastClient.py
+++ doc/core/howto/listings/udp/MulticastClient.py
@@ -1,12 +1,29 @@
 from twisted.internet.protocol import DatagramProtocol
 from twisted.internet import reactor
+from twisted.python import log
+import sys
 
-class MulticastClientUDP(DatagramProtocol):
+# For a client to send multicast to a multicast server it needs to join
+# the same multicast address
+mcastaddr = '228.0.0.5'
+
+class MulticastPingClient(DatagramProtocol):
+
+    def startProtocol(self):
+        log.msg("TTL: " + repr(self.transport.getTTL()))
+        # Default TTL=1 to send over router hops TTL needs to be increased
+        self.transport.setTTL(3)
+        # send IGMP JOIN for multicast group addr so you can receive
+        self.deferred=self.transport.joinGroup(mcastaddr)
+        # send to 228.0.0.5:8005 - all listeners will receive - i.e multicast!
+        # You could also send unicast to server via write('data',(addr,8005))
+        self.transport.write('Client1: Ping',(mcastaddr, 8005))
 
     def datagramReceived(self, datagram, address):
-            print "Received:" + repr(datagram)
+        log.msg("Received: " + repr(datagram))
 
-# Send multicast on 224.0.0.1:8005, on our dynamically allocated port
-port = reactor.listenUDP(0, MulticastClientUDP())
-port.write('UniqueID', ('224.0.0.1', 8005))
+# listen on multicast 228.0.0.1:8005, just like the server
+log.startLogging(sys.stdout)
+reactor.listenMulticast(8005, MulticastPingClient())
+
 reactor.run()
--- doc/core/howto/listings/udp/MulticastServer.py
+++ doc/core/howto/listings/udp/MulticastServer.py
@@ -1,24 +1,34 @@
 from twisted.internet.protocol import DatagramProtocol
 from twisted.internet import reactor
+from twisted.python.log import msg, err, startLogging
+import sys
 
-class MulticastServerUDP(DatagramProtocol):
+# Multicast address has to be in range 224.0.0.0 - 239.255.255.255
+# Multicast addr in range 224.0.0.0-255 DON'T pass 1st router with ANY TTL(ie > 1)
+# Multicast addr 224.0.0.1 (all hosts) is joined by kernel so NO IGMP sent
+mcastaddr = "228.0.0.5"
+
+class MulticastPingPong(DatagramProtocol):
+
+    def __init__(self, text):
+        self.text = text
+
     def startProtocol(self):
-        print 'Started Listening'
+        # Need to set the TTL so multicast will cross router hops
+        self.transport.setTTL(5)
         # Join a specific multicast group, which is the IP we will respond to
-        self.transport.joinGroup('224.0.0.1')
+        self.deferred=self.transport.joinGroup(mcastaddr)
 
     def datagramReceived(self, datagram, address):
-        # The uniqueID check is to ensure we only service requests from
-        # ourselves
-        if datagram == 'UniqueID':
-            print "Server Received:" + repr(datagram)
-            self.transport.write("data", address)
+        msg("Datagram %s received from %s" % (repr(datagram), repr(address)))
+        if datagram == 'Client1: Ping':
+            self.transport.write("Srv: " + self.text, (mcastaddr, 8005))
 
-# Note that the join function is picky about having a unique object
-# on which to call join.  To avoid using startProtocol, the following is
-# sufficient:
-#reactor.listenMulticast(8005, MulticastServerUDP()).join('224.0.0.1')
 
-# Listen for multicast on 224.0.0.1:8005
-reactor.listenMulticast(8005, MulticastServerUDP())
+startLogging(sys.stdout)
+# start multicast listener on multiple interfaces
+# To avoid using startProtocol, the following is sufficient:
+# reactor.listenMulticast(8005, MulticastPingPong()).joinGroup(mcastaddr)
+
+reactor.listenMulticast(8005, MulticastPingPong("Mcast Pong"), listenMultiple=True)
 reactor.run()
