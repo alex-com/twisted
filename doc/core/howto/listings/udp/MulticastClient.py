d12b9f5f031dc96f2e22e1cb6e31ba5770c01314
itamarst <itamarst@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2011-11-10 03:08:41 +0000

    Editing and simplification.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/multicast-doc-4262@33102 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- doc/core/howto/listings/udp/MulticastClient.py
+++ doc/core/howto/listings/udp/MulticastClient.py
@@ -1,29 +1,20 @@
 from twisted.internet.protocol import DatagramProtocol
 from twisted.internet import reactor
-from twisted.python import log
-import sys
 
-# For a client to send multicast to a multicast server it needs to join
-# the same multicast address
-mcastaddr = '228.0.0.5'
 
 class MulticastPingClient(DatagramProtocol):
 
     def startProtocol(self):
-        log.msg("TTL: " + repr(self.transport.getTTL()))
-        # Default TTL=1 to send over router hops TTL needs to be increased
-        self.transport.setTTL(3)
-        # send IGMP JOIN for multicast group addr so you can receive
-        self.deferred=self.transport.joinGroup(mcastaddr)
-        # send to 228.0.0.5:8005 - all listeners will receive - i.e multicast!
-        # You could also send unicast to server via write('data',(addr,8005))
-        self.transport.write('Client1: Ping',(mcastaddr, 8005))
+        # Join the multicast address, so we can receive replies:
+        self.transport.joinGroup("228.0.0.5")
+        # Send to 228.0.0.5:8005 - all listeners on the multicast address will
+        # receive this message. We also send unicast to a specific address the
+        # same way a normal UDP client would.
+        self.transport.write('Client: Ping', ("228.0.0.5", 8005))
 
     def datagramReceived(self, datagram, address):
-        log.msg("Received: " + repr(datagram))
+        print "Received: " + repr(datagram)
 
-# listen on multicast 228.0.0.1:8005, just like the server
-log.startLogging(sys.stdout)
-reactor.listenMulticast(8005, MulticastPingClient())
 
+reactor.listenMulticast(8005, MulticastPingClient(), listenMultiple=True)
 reactor.run()
--- doc/core/howto/listings/udp/MulticastServer.py
+++ doc/core/howto/listings/udp/MulticastServer.py
@@ -1,34 +1,28 @@
 from twisted.internet.protocol import DatagramProtocol
 from twisted.internet import reactor
-from twisted.python.log import msg, err, startLogging
-import sys
 
-# Multicast address has to be in range 224.0.0.0 - 239.255.255.255
-# Multicast addr in range 224.0.0.0-255 DON'T pass 1st router with ANY TTL(ie > 1)
-# Multicast addr 224.0.0.1 (all hosts) is joined by kernel so NO IGMP sent
-mcastaddr = "228.0.0.5"
 
 class MulticastPingPong(DatagramProtocol):
 
-    def __init__(self, text):
-        self.text = text
-
     def startProtocol(self):
-        # Need to set the TTL so multicast will cross router hops
+        """
+        Called after protocol has started listening.
+        """
+        # Set the TTL>1 so multicast will cross router hops:
         self.transport.setTTL(5)
-        # Join a specific multicast group, which is the IP we will respond to
-        self.deferred=self.transport.joinGroup(mcastaddr)
+        # Join a specific multicast group:
+        self.transport.joinGroup("228.0.0.5")
 
     def datagramReceived(self, datagram, address):
-        msg("Datagram %s received from %s" % (repr(datagram), repr(address)))
-        if datagram == 'Client1: Ping':
-            self.transport.write("Srv: " + self.text, (mcastaddr, 8005))
+        print "Datagram %s received from %s" % (repr(datagram), repr(address))
+        if datagram == "Client: Ping":
+            # Rather than replying to the group multicast address, we send the
+            # reply directly to the originating port:
+            self.transport.write("Server: Pong", address)
 
 
-startLogging(sys.stdout)
-# start multicast listener on multiple interfaces
-# To avoid using startProtocol, the following is sufficient:
-# reactor.listenMulticast(8005, MulticastPingPong()).joinGroup(mcastaddr)
-
-reactor.listenMulticast(8005, MulticastPingPong("Mcast Pong"), listenMultiple=True)
+# We use listenMultiple=True so that we can run MulticastServer.py and
+# MulticastClient.py on same machine:
+reactor.listenMulticast(8005, MulticastPingPong(),
+                        listenMultiple=True)
 reactor.run()
