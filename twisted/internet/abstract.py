3321e7e58d95855e4e5db6b2316e22f836c25859
itamarst <itamarst@bbbe8e31-12d6-0310-92fd-ac37d47ddeeb>
2011-11-14 00:17:09 +0000

    Fix review comment.
    git-svn-id: svn://svn.twistedmatrix.com/svn/Twisted/branches/select-halfclose-3037@33141 bbbe8e31-12d6-0310-92fd-ac37d47ddeeb

--- twisted/internet/abstract.py
+++ twisted/internet/abstract.py
@@ -259,7 +259,7 @@ class FileDescriptor(_ConsumerMixin, _LogOwner):
                 # so.
                 return self._postLoseConnection()
             elif self._writeDisconnecting:
-                # I was previously asked to to half-close the connection.  We
+                # I was previously asked to half-close the connection.  We
                 # set _writeDisconnected before calling handler, in case the
                 # handler calls loseConnection(), which will want to check for
                 # this attribute.
